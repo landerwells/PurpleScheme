b0bc2f2bccd569dd1cbec3b0d2306b5841702f05
landerwells <landerwells@gmail.com>
2024-07-27 19:43:05 -0500

    added comma

--- lua/purple/theme.lua
+++ lua/purple/theme.lua
@@ -26,7 +26,7 @@ function M.setup()
 
   theme.highlights = {
     Comment = { fg = "#51545c", style = options.styles.comments }, -- any comment
-    ColorColumn = { bg = c."#808080" }, -- used for the columns set with 'colorcolumn'
+    ColorColumn = { bg = "#808080" }, -- used for the columns set with 'colorcolumn'
     Conceal = { fg = c.blue500 }, -- placeholder characters substituted for concealed text (see 'conceallevel')
     Cursor = { fg = c.base03, bg = c.base0 }, -- character under the cursor
     lCursor = { fg = c.base03, bg = c.base00 }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
