a9fc5e767405acbdf14033a3625a7ad62dafe239
landerwells <landerwells@gmail.com>
2024-08-01 13:10:28 -0500

    lualine

--- lua/lualine/themes/purple.lua
+++ lua/lualine/themes/purple.lua
@@ -4,9 +4,9 @@ local config = require("purple.config").options
 local purple_local = {}
 
 purple_local.normal = {
-  a = { bg = "#000000", fg = "#ffffff" },
-  b = { bg = "#ffffff", fg = "#171717" },
-  c = { bg = "#1d1d1d", fg = colors.fg },
+  a = { bg = "#ffffff", fg = "#000000" },
+  b = { bg = "#171717", fg = "#ffffff" },
+  c = { bg = "#1d1d1d", fg = "#1d1d1d" },
 }
 
 purple_local.insert = {
