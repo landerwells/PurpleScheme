6b398f6b97a98e7393e61eaec0e33f4d0773efc8
landerwells <landerwells@gmail.com>
2024-08-10 20:47:27 -0500

    removing reverse for last time

--- lua/purple/theme.lua
+++ lua/purple/theme.lua
@@ -70,8 +70,8 @@ function M.setup()
     PmenuThumb = { fg = c.base01, reverse = true }, -- Popup menu: Thumb of the scrollbar.
     Question = { fg = c.cyan100, bold = true }, -- |hit-enter| prompt and yes/no questions
     QuickFixLine = { bg = c.blue100, bold = true }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-    Search = { bg = "#4f4f4f", reverse = true }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
-    IncSearch = { bg = "#4f4f4f" }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+    Search = { bg = "#2f2f2f" }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
+    IncSearch = { bg = "#2f2f2f" }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
     CurSearch = { link = "IncSearch" },
     SpecialKey = { fg = c.base01 }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
     SpellBad = { sp = c.red500, undercurl = true }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
